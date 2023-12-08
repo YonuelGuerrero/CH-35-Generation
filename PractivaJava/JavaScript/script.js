f33e4db43bb92e7e1060fe28a773d346aa27b8b7
YonuelGuerrero <yonuelguerrero@gmail.com>
2023-12-08 16:16:29 -0600

    practicas

---
+++ PractivaJava/JavaScript/script.js
@@ -0,0 +1,56 @@
+//Comentarios de una sola linea
+
+/*comentarios 
+multilinea*/
+
+//alert ("Hola mundo");
+
+//console.log ("Mensaje para la consola");
+
+/* var nombre = prompt("Ingresa tu nombre");
+alert ("Hola, "+ nombre+"  !Bienvenido a la CH35!"); */
+
+/*var numero1 = parseInt(prompt("Ingresa el primer numero:"))
+var numero2 = parseInt(prompt("Ingresa el segundo numero:"))
+
+console.log("suma:" + (numero1 + numero2));
+console.log("resta:" + (numero1 - numero2));
+console.log("multiplicacion:" + (numero1 * numero2));
+console.log("division:" + (numero1 / numero2));
+*/
+
+/* var nombre = prompt("Ingresa tu nombre");
+var edad = prompt("ingresa tu edad");
+
+    if (edad >=18)
+{
+  alert("Ya tas ruco, pasale")
+}
+if(edad <18)
+{
+  alert("No puedes tas chikito")
+}
+*/
+
+
+//TIPOS DE DATOS Y VARIABLES
+
+//scope es el alcance de nuestras variables
+
+/*var, let, const la diferencia es el alcance 
+var tiene un alcance global
+let tiene un alcnace local
+const se mantiene sin ningun cambio*/
+
+/*string:cadena de texto (lleva comillas)
+number: numero
+noolena: true/false
+null:nulo
+undefinied:no esta asigando o definido*/
+
+let nombre= "Daniel";
+let invitaxsExtra = 2;
+let esMayorEdad = true;
+let edad = "24";
+let invitadxEspecial = "null";
+let horaDeSalida = undefined;
